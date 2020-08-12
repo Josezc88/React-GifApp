c6d1af98a5045961df3e71de89dd6e650efc087d
Jose Luis Caldera <jluiscaldera@hotmail.com>
2020-08-12 13:29:53 -0500

    Docs Agregado

---
+++ docs/precache-manifest.fa534aa524c190f7579caa65db0b1afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dd3e0d1d910889758515f1811c0c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74decff149909909e106",
+    "url": "/static/css/main.035fa975.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74decff149909909e106",
+    "url": "/static/js/main.52280d2c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
